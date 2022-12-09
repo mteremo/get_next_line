ed5ad26563c54fef104b4cb940d7c3d4c9ad32dd
Maria Teresa Tavares De Sousa Mota <matavare@c1r7s5.42lisboa.com>
2022-12-09 19:57:43 +0000

    starting

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matavare <matavare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 18:09:50 by matavare          #+#    #+#             */
+/*   Updated: 2022/12/09 19:38:12 by matavare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
